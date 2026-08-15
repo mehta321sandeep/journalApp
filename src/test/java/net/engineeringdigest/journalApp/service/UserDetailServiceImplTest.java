83d0d38a2f13c046cd8d16a99137bdfe72a6a450
SANDEEP KUMAR <sk321ssm@gmail.com>
2026-08-15 15:45:56 +0530

    first commit

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailServiceImplTest.java
@@ -0,0 +1,44 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class UserDetailServiceImplTest {
+
+    @InjectMocks
+    private UserDetailServiceImpl userDetailService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp()
+    {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("sudyb").roles(new ArrayList<>()).build());
+        UserDetails user=userDetailService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
